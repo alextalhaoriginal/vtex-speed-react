8cd1f336d07e2a02ce07d3e19eeb1fdcf96988aa
Raphael Corrêa <raphael.correa.b@gmail.com>
2019-09-17 01:46:15 -0300

    Primeiro commit

---
+++ src/js/modules/home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+/**
+ * Componente 
+ */
+const MainTitle = (props) => {
+    return <h1>Olá mundo, essa é minha loja {props.name}!</h1>;
+}
+
+
+/**
+ *  Renderizar o compoente no container title
+ */
+ReactDOM.render(
+    <MainTitle name="SapatosLindos.com" />, 
+    document.getElementById("title")
+);
---
+++ src/js/modules/menu.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import axios from 'axios'
+
+const APIMenuTree = '/api/catalog_system/pub/category/tree/1'
+
+export default class Menu extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            menuList: [],
+            isLoaded: false
+        }
+    }
+
+    componentDidMount() {
+        axios.get(APIMenuTree).then((resp) => {
+            this.setState({ menuList: resp.data, isLoaded: true })
+        });
+    }
+
+    render() {
+        if (this.state.isLoaded) {
+            return (
+                <menu>
+                    <ul>
+                        {
+                            this.state.menuList.map((el, i) => {
+                                return (
+                                    <li key={i}><a href={el.url} titel={el.Title}> {el.name} </a> </li>
+                                )
+                            })
+                        }
+                    </ul>
+                </menu>
+            );
+        } else {
+            return (<li>Carregando ...</li>)
+        }
+    }
+}
+
+ReactDOM.render(
+    <Menu />,
+    document.getElementById("menu")
+);
